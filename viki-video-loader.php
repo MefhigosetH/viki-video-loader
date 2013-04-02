6d6791d4294f8d4ab233289ee91a21961e1ffd24
MefhigosetH <mefhigoseth@gmail.com>
2013-04-01 22:14:23 -0300

    viki-video-loader.php:\n* Now the script can 'follow' the next chapters in a channel.\n* You can select the quality of the video before download each chapter.

--- viki-video-loader.php
+++ viki-video-loader.php
@@ -41,80 +41,87 @@ echo "  Viki.com video downloader\n";
 echo "         Coded by Mefh!\n";
 echo "====================================\n";
 
-$viki_lang = promptUser("Lenguaje","es");
+$viki_lang = promptUser("Language","es");
 $viki_id = promptUser("Video ID");
 
-echo "Obteniendo informacion ... ";
-$viki_info = file_get_contents("http://www.viki.com/player/medias/".$viki_id."/info.json");
+do {
+	echo "Obtaining information ... ";
+	$viki_info = file_get_contents("http://www.viki.com/player/medias/".$viki_id."/info.json");
 
-if( $viki_info === FALSE ) {
-	die("Error.\r\n");
-}
+	if( $viki_info === FALSE ) {
+		die("Error.\r\n");
+	}
 
-echo "Ok.\r\n";
-$viki_json = json_decode($viki_info,TRUE);
-//print_r($viki_json);
-echo "\r\nTitulo: ".$viki_json['title']."\r\n";
-echo "Descripcion: ".$viki_json['description']."\r\n\r\n";
+	$viki_json = json_decode($viki_info,TRUE);
+	if( $viki_json === FALSE ) {
+		die("Error.\r\n");
+	}
+	echo "Ok.\r\n";
 
-promptUser("Presione 'Enter' para continuar");
-
-
-echo "Descargando subtitulos ... ";
-$viki_ljson = file_get_contents("http://www.viki.com/subtitles/media/".$viki_id."/".$viki_lang.".json");
-
-if( $viki_ljson === FALSE ) {
-	die("Error.\r\n");
-}
-
-echo "Ok.\r\n";
-echo "Convirtiendo JSON a STR... ";
-
-$viki_array = json_decode($viki_ljson,TRUE);
-echo "Ok.\r\n";
-//print_r($viki_array);
-
-echo "Creando archivo srt... ";
-if( !($fp = fopen($viki_id."-".$viki_lang.".srt", "a+")) ) {
-	die("Error.\r\n");
-}
-
-$c = 1;
-
-foreach($viki_array['subtitles'] as $srt => $srtData) {
-	fwrite($fp,$c."\r\n");
-	$timeStart = date("H:i:s", mktime(0, 0, substr($srtData['start_time'],0,-3), 1, 1, 2000));
-	$timeStart .= ",".substr($srtData['start_time'],-3);
-	$timeEnd = date("H:i:s", mktime(0, 0, substr($srtData['end_time'],0,-3), 1, 1, 2000));
-	$timeEnd .= ",".substr($srtData['end_time'],-3);
-	fwrite($fp,$timeStart." --> ".$timeEnd."\r\n");
-	fwrite($fp,$srtData['content']."\r\n\r\n");
-	$c++;
-}
-
-echo "Ok.\r\n";
-fclose($fp);
-
-$c = 0;
-
-while(!$c) {
-	$viki_quality = promptUser("Seleccione la calidad del video (720p, 480p, 360p, 240p)","720p");
-
+	$viki_title = $viki_json['title'];
+	$viki_description = $viki_json['description'];
+	$viki_next_title = $viki_json['next_video']['title'];
+	$viki_next_id = $viki_json['next_video']['media_id'];
 	foreach($viki_json['streams'] as $stream => $streamData) {
-		if( $streamData['quality'] == $viki_quality ) {
-			$c = 1;
-		}
+		$viki_uris[$streamData['quality']] = $streamData['uri'];
 	}
 
-	if($c) {
-		echo "Descargando video ... \r\n";
-		//system("wget ".$viki_json['streams'][0]['uri']);
+	unset($viki_info);
+	unset($viki_json);
+
+	echo "\r\nThe title: ".$viki_title."\r\n";
+	echo "The description: ".$viki_description."\r\n\r\n";
+
+	do {
+		$qty = promptUser("Select quality of video:",implode(",",array_keys($viki_uris)));
 	}
-	else {
-		echo "Calidad no disponible!\r\n";
+	while( !isset($viki_uris[$qty]) );
+
+	echo "Descargando video ... \r\n";
+	system("wget ".$viki_uris[$qty]);
+
+	echo "\r\nDownloading subtitles ... ";
+	$viki_ljson = file_get_contents("http://www.viki.com/subtitles/media/".$viki_id."/".$viki_lang.".json");
+
+	if( $viki_ljson === FALSE ) {
+		die("Error.\r\n");
 	}
+
+	echo "Ok.\r\n";
+	echo "Converting JSON to STR filetype... ";
+
+	$viki_array = json_decode($viki_ljson,TRUE);
+	if( $viki_array === FALSE ) {
+		die("Error.\r\n");
+	}
+	echo "Ok.\r\n";
+
+	echo "Building srt file... ";
+	$srtFile = explode("/",$viki_uris[$qty]);
+	if( !($fp = fopen($srtFile[4].".srt", "a+")) ) {
+		die("Error.\r\n");
+	}
+
+	$c = 1;
+
+	foreach($viki_array['subtitles'] as $srt => $srtData) {
+		fwrite($fp,$c."\r\n");
+		$timeStart = date("H:i:s", mktime(0, 0, substr($srtData['start_time'],0,-3), 1, 1, 2000));
+		$timeStart .= ",".substr($srtData['start_time'],-3);
+		$timeEnd = date("H:i:s", mktime(0, 0, substr($srtData['end_time'],0,-3), 1, 1, 2000));
+		$timeEnd .= ",".substr($srtData['end_time'],-3);
+		fwrite($fp,$timeStart." --> ".$timeEnd."\r\n");
+		fwrite($fp,$srtData['content']."\r\n\r\n");
+		$c++;
+	}
+
+	echo "Ok.\r\n";
+	fclose($fp);
+
+	echo "The next chapter is: ".$viki_next_title."\r\n";
+	$viki_id = $viki_next_id;
+	$ans = promptUser("Do you want to download the next chapter too?","y");
+	$ans = strtolower($ans);
 }
-
-echo "El proximo video es: ".$viki_json['next_video']['title']." (".$viki_json['next_video']['media_id'].")\r\n";
-
+while($ans=="y");
 ?>
